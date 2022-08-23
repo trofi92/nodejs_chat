43a59f06f7019c24893ef3df22adc5180e378104
trofi92 <84131398+trofi92@users.noreply.github.com>
2022-08-23 20:24:14 +0900

    messenger(php=lampp)

--- index.js
+++ index.js
@@ -1,6 +1,53 @@
 const WebSocket = require("ws");
-const ws = WebSocket.Server({port:8008});
+const ws = new WebSocket.Server({port:8008});
 
-ws.on ("connertion", function connect(ws,req){  //웹소켓에 특정 클라이언트가 연결 되었을때 실행
- 
+let user_id = 0; /**클라이언트에게 부여되는 고유값 */
+let ALL_WS = []; /**전체 유저 통제, 웹소켓과 아이디 저장 */
+ws.on ("connection", function connect(websocket,req){  /**웹소켓에 특정 클라이언트가 연결 되었을때 실행*/
+    user_id++;
+    console.log("NEW USER CONNECT (" + user_id + ")")
+    ALL_WS.push({"ws":websocket, "user_id":user_id, "user_name":""})
+
+    sendUserId(user_id);
+    websocket.on("close", function close(code,reason) {
+         ALL_WS.forEach(function (element, index) {
+                    if (element.ws == websocket) { /**접속이 끊긴 유저 */
+                        ALL_WS.splice(index, 1);
+                    }
+                });
+                sendAllUsers();
+    });
+    websocket.on("message", function incoming(message) {
+        console.log(JSON.parse(message));
+        message = JSON.parse(message);
+        switch (message.code) {
+            case "connect_name": /** 사용자 추가 */
+                ALL_WS.forEach(function (element, index) {
+                    if (element.user_id == message.user_id) {
+                        element.user_name = message.name;
+                    }
+                });
+                sendAllUsers();
+                break;
+            case "send_message": /** 채팅메시지 받음 */
+                ALL_WS.forEach(function (element, index) {
+                    /**element.ws = 클라이언트와의 연결 지점 */
+                    let data = {"code":"chat_message","msg":message.msg, "sender_name": message.name};
+                    element.ws.send(JSON.stringify(data));
+                });
+                break;
+        }
+    })
+
+    function sendAllUsers() {  /**전체 사용자 정보를 보냄 */
+        let data = {"code":"all_users","msg":JSON.stringify(ALL_WS)};
+        ALL_WS.forEach(function (element, index) {
+            element.ws.send(JSON.stringify(data));
+                })
+    }
+
+    function sendUserId(user_id) {
+        let data = { "code": "my_user_id", "msg": user_id }
+        websocket.send(JSON.stringify(data)); /** 웹소켓 : 서버와 클라이언트 간 데이터 교환의 매개 =>*/
+    }
 });
